ab7e28dfb11cf2ff0025eb31ba35d32f3060dbe4
jacobmh1177 <jacobmh@stanford.edu>
2017-05-23 10:03:05 +0000

    fixed weather

--- src/weather.js
+++ src/weather.js
@@ -1,7 +1,16 @@
 "use strict";
 
 var DarkSky = require('forecast.io');
-const util = require('util');
+
+var edison = require('edison-oled');
+var display = new edison.Oled();
+//setup buttons
+var btnUp = new edison.Gpio(47, edison.INPUT);
+var btnDown = new edison.Gpio(44, edison.INPUT);
+var btnLeft = new edison.Gpio(165, edison.INPUT);
+var btnRight = new edison.Gpio(45, edison.INPUT);
+var btnSelect = new edison.Gpio(48, edison.INPUT);
+var btnA = new edison.Gpio(49, edison.INPUT);
 
 var options = {
   APIKey: '8581aa6250437a4b012c73d2beeb3130',
@@ -15,16 +24,19 @@ function WeatherForecast() {
         this.HOME_LONG = -122.170099;
 }
 
-WeatherForecast.prototype.getCurrentForecast = function(){
-        var forecast;
-        darksky.get(HOME_LAT, HOME_LONG, function (err, res, data) {
+WeatherForecast.prototype.getCurrentForecast = function(callback){
+        darksky.get(this.HOME_LAT, this.HOME_LONG, function (err, res, data) {
                 if (err) throw err;
-                this.currentForecast = data.currently;
-                forecast = currentForecast;
-        });
-        return forecast;
+        	callback(data);
+	});
+}
+
+function display(data) {
+	
 }
 
 var forecast = new WeatherForecast();
-console.log(forecast.getCurrentForecast);
+forecast.getCurrentForecast(function(data) {
+	console.log(data)
+});
 
