a3414f62ebfe18dae9db9f0e9e45b062d0a3f9bd
jacobmh1177 <jacobmh@stanford.edu>
2017-05-29 01:19:08 -0700

    added scrolling

--- src/menu.js
+++ src/menu.js
@@ -42,27 +42,40 @@ function displayMenu2(menuOptions, boxY) {
 
 function Menu(menuOptions, selected, leftBreakout) {
 	var maxDisplayable = 5;
+	var maxOption = menuOptions.length - 1;
 	var displayableMenu = menuOptions;
 	if (menuOptions.length > maxDisplayable) {
-		displayableMenu = menuOptions.splice(1, maxDisplayable);
+		displayableMenu = menuOptions.slice(0, maxDisplayable);
 	}
 	var boxY = (selected) * 10;
 	var yMax = (displayableMenu.length - 1) * 10;
         displayMenu2(displayableMenu, boxY);
+	var topSelected = 0;
 	while (true) {
+
                 if (btnDown.pinRead() == edison.LOW) {
-                        if (boxY < yMax) {
-                                boxY += 10;
-                                selected += 1;
-                        }
+                        if (boxY <= yMax) {
+                                if (boxY < yMax) boxY += 10;
+                                if (selected < maxOption) selected += 1;
+			}
+			if (selected > displayableMenu.length - 1) {
+				displayableMenu = menuOptions.slice(selected - maxDisplayable + 1, selected + 2);
+				topSelected = selected - maxDisplayable + 1;
+				console.log("scroll down");
+			}	
                         displayMenu2(displayableMenu, boxY);
+			console.log("selected = ", menuOptions[selected]);
                 }
                 if (btnUp.pinRead() == edison.LOW) {
-                	if (boxY > 0) {
-                                boxY -= 10;
-                                selected -= 1;
+                	if (boxY >= 0) {
+                                if (boxY > 0) boxY -= 10;
+                                if (selected > 0) selected -= 1;
                         }
+			if (selected === topSelected - 1 && topSelected !== 0) {
+				displayableMenu = menuOptions.slice(selected, topSelected + maxDisplayable);
+			}	
                         displayMenu2(displayableMenu, boxY);
+			console.log("selected = ", menuOptions[selected]);
                 }
                 if (btnRight.pinRead() == edison.LOW) {
                         break;
