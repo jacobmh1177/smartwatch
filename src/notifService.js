08946954a0919e3ba513738139b94fe0168a0ed6
jacobmh1177 <jacobmh@stanford.edu>
2017-05-30 10:04:15 -0700

    Synced notfication list in notifService.js

--- src/notifService.js
+++ src/notifService.js
@@ -1,4 +1,4 @@
-var BleAncs = require('ble-ancs');
+var BleAncs = require('/home/root/smartwatch/src/ble-ancs/index.js');
 var ancs = new BleAncs();
 
 var fs = require('fs');
@@ -8,10 +8,22 @@ var exec = require('child_process').execSync;
 var ANCS_STORE_FN = "/home/root/smartwatch/src/notif.txt";
 var notificationList = [];
 
+function syncList() {
+	var text = fs.readFileSync(ANCS_STORE_FN, "utf8");
+	var stringNotificationList = text.split('\n');
+	var notificationList = [];
+	stringNotificationList.forEach(function(value) {
+		if (Value.length > 0) {
+			notificationList.push(JSON.parse(value));
+		}
+	});
+	return notificationList;
+}
 exec('rfkill unblock bluetooth');
 ancs.on('notification', function(notification) {
 	notification.readTitle(function(title) {
 		notification.readMessage(function(message) {
+			notifcationList = syncList();
 			notificationList.push(notification);
 			fs.writeFileSync(ANCS_STORE_FN, "");
 			notificationList.forEach(function(notif) {
@@ -20,3 +32,4 @@ ancs.on('notification', function(notification) {
 		});
 	});
 });
+console.log("in notif service");
